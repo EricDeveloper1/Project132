10a6035b33562286952f8c3547015a5b39842312
EricDeveloper1 <128795646+EricDeveloper1@users.noreply.github.com>
2024-02-24 18:06:00 +0400

    Add files via upload

---
+++ workroom.js
@@ -0,0 +1,36 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage("workroom.jpg");
+
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    status = true;
+    console.log("Model Loaded")
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+}
+
+function back() {
+    window.location = "index.html";
+}
